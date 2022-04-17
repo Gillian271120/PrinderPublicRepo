0edffa7e1d214be302bf969f9f83b7a55fcb1631
unknown <Gillian271120@gmail.com>
2022-04-17 18:24:13 +0100

    Añadido funcionalidad de Productos y página 404

--- backend/templates/Products/index.php
+++ backend/templates/Products/index.php
@@ -6,16 +6,6 @@
     </tr>
 
     <!-- Here is where we iterate through our $articles query object, printing out article info -->
-
-    <?php
-
-use Cake\ORM\TableRegistry;
-
-    $productTable = TableRegistry::getTableLocator()->get('Products');
-    $productTable->find(); 
-    debug($productTable->find()->toArray());exit;
-    ?>
-
     <?php foreach ($products as $product): ?>
     <tr>
         <td>
